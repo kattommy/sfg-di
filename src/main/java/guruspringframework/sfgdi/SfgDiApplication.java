085ebc556bd5e62de1e66faee47aaad1d8cf18e2
Katarzyna Tomczak <katarzyna.ntomczak@gmail.com>
2022-12-29 09:57:17 +0100

    add pom and .gitignore and application properties

---
+++ src/main/java/guruspringframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,36 @@
+package guruspringframework.sfgdi;
+
+import guruspringframework.sfgdi.controllers.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication(scanBasePackages = {"guruspringframework.sfgdi", "guruspringframework.sfgdi.controllers"})
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		System.out.println("--------------------Primary bean");
+		System.out.println(myController.sayHello());
+
+		System.out.println("--------------------Property");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("----------------------Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("--------------------Contrustyor");
+
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+	}
+}
